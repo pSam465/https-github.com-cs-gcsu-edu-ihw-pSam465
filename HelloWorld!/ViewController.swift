c9d8a4c6cd8bdef39ea306dfc5e3ec33f510e61a
sam <pittmansam64@gmail.com>
2021-02-22 14:36:36 -0500

    added the app stuff

--- HelloWorld!/ViewController.swift
+++ HelloWorld!/ViewController.swift
@@ -9,6 +9,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var txtName: UITextField!
+    @IBOutlet weak var lblOutlet: UILabel!
+    @IBAction func showOutput(_ sender: Any)
+    {
+        let name = txtName.text
+        let output = "Hello " + name!
+        lblOutlet.text = output
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
